3dc94a92d3dad60aed13894914720d2817ba8571
Bob Weigel <Robert.S.Weigel@gmail.com>
2020-08-11 11:02:44 -0400

    Add notes to python script

--- scripts/python.py
+++ scripts/python.py
@@ -4,21 +4,32 @@
 # Report data server issues to CONTACT
 
 # Install latest hapiclient package from https://pypi.org/project/hapiclient/
+# Only needs to be executed once.
 import os; print(os.popen('pip install hapiclient --upgrade').read())
 
 from hapiclient import hapi, hapiplot
 
 server     = 'SERVER';
 dataset    = 'DATASET';
-# Use parameters='' to request all data. Multiple parameters
-# can be requested using a comma-separated listCSV_EXAMPLE
+# Notes:
+# 1. Use parameters='' to request all parameters from DATASET.
+# 2. Multiple parameters can be requested using a comma-separated
+#    listCSV_EXAMPLE
 parameters = 'PARAMETERS'; 
 start      = 'START';
 stop       = 'STOP';
 
-help(hapi)
 data, meta = hapi(server, dataset, parameters, start, stop)
-print(data)
-print(meta)
 
 hapiplot(data, meta)
+
+# Notes:
+# 1. To convert the ISO 8601 strings in data['TIMENAME'] to Python
+#    datetimes, use
+#      Time = hapitime2datetime(data['TIMENAME'])
+# 2. Details about the data and metadata structures `data`
+#    and `meta` are given at 
+#    https://github.com/hapi-server/client-python-notebooks/blob/master/hapi_demo.ipynb
+# 3. Many examples for using `data` and `meta` with other
+#    Python libraries (e.g., Pandas, Numpy, Astropy) are given
+#    in above-referenced notebook.

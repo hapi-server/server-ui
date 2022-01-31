fb52afcec59facb9fef614dd7d69c22e63f66b71
Bob Weigel <rweigel@gmu.edu>
2022-01-31 15:55:11 -0500

    wget and curl

---
+++ scripts/curl.sh
@@ -0,0 +1,2 @@
+curl "SERVER/data?id=DATASET&parameters=PARAMETERS&time.min=START&time.max=STOP" > \
+    "DATASET_PARAMETERS_START-STOP.csv"
---
+++ scripts/wget.sh
@@ -0,0 +1,3 @@
+wget "SERVER/data?id=DATASET&parameters=PARAMETERS&time.min=START&time.max=STOP" \
+    -O "DATASET_PARAMETERS_START-STOP.csv"
+

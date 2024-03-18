bde3fbf2e150729b974e6303d330d4aee773a331
Bob Weigel <rweigel@gmu.edu>
2024-03-17 21:35:22 -0400

    correct logic for 2to3

--- js/util.js
+++ js/util.js
@@ -39,10 +39,11 @@ let util = {
   },
 
   hapi2to3: function (url, version) {
+
     if (!version) {
       version = datasets.json.HAPI || "2.0";
     }
-    if (version.substr(0,1) == "2") {
+    if (version.substr(0,1) == "3") {
       url.replace("/info?id=","/info?dataset=");
       url.replace("/time.min=","/start=");
       url.replace("/time.max=","/stop=");
